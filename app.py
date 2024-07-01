3be73629dd27c9649e87105e97cdc8b65fd5d901
Allen Antony <allenantony26@gmail.com>
2024-07-01 22:51:53 +0100

    localhost flask app running

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import config
+
+config = config.CONFIG
+app = Flask(__name__)
+app.config['SECRET'] = config['SECRET'] #for trial purpose using this will be changed later
+socketio = SocketIO(app, cors_allowed_origin='*')
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    socketio.run(app, host="localhost")
