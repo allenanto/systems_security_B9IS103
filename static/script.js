da56ed0a1dbca7b169c0dbacd55e30c86cde8e4a
Allen Antony <allenantony26@gmail.com>
2024-07-01 23:53:45 +0100

    app.py connection handle

--- static/script.js
+++ static/script.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
     var socket = io.connect("https://localhost:5000")
     socket.on('connect', function(){
-        socket.send("connection_successful")
+        console.log("Connected to server")
+        socket.send("connection_successful");
     });
-    
+
+    socket.on('message', function(message){
+        $('#messages').append($('<p>').text(data));
+    });
+
+    $('sendBtn').on('click', function(){
+        socket.send($('#username').val() + ': ' + $('#message').val());
+    })
 });
