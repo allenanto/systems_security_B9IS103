be5584cc7c1179adf44c08e255357abc3c75cf38
Allen Antony <allenantony26@gmail.com>
2024-07-02 00:06:12 +0100

    updates

--- static/script.js
+++ static/script.js
@@ -1,16 +1,12 @@
 $(document).ready(function(){
     var socket = io.connect("http://localhost:5000")
     socket.on('connect', function(){
-        console.log("Connected to server")
+        console.log("Connected to server");
         socket.send("connection_successful");
     });
 
-    socket.on('message', function(message){
-        $('#messages').append($('<p>').text(data));
-    });
-
     $('sendBtn').on('click', function(){
-        console.log("message send:"+ $('#username').val())
+        console.log("message send:");
         socket.send($('#username').val() + ': ' + $('#message').val());
     })
 });
