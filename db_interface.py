5420550ae2aebf1a21de2fbeff95acfa1a539610
swayakrishna <165483828+swayakrishna@users.noreply.github.com>
2024-07-10 20:13:21 +0000

    Creating table

--- db_interface.py
+++ db_interface.py
@@ -2,7 +2,21 @@ import sqlite3
 
 def init_db():
     conn = sqlite3.connect('corptalk.db')
-    print("db connection")
+
+    #Creating User table
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            mobile TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+
+    print("Db createdn")
+
+def create_user()
 
 if __name__ == "__main__":
     init_db()
