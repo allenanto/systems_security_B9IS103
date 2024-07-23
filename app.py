8f31d38157b2c54704234be739b0d93afe613a70
Tharunsai911 <143305003+Tharunsai911@users.noreply.github.com>
2024-07-23 00:06:20 +0000

    updates

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def handle_message(message):
         send(message, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app, host="localhost")
+    socketio.run(app, host='0.0.0.0', port=5000)
