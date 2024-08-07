070ac8c02a47dd56b03bc87052bf5663c4a7e468
swayakrishna <165483828+swayakrishna@users.noreply.github.com>
2024-08-07 16:22:14 +0000

    RSA

--- keys.py
+++ keys.py
@@ -7,3 +7,11 @@ from Crypto.Cipher import PKCS1_OAEP
 def generate_keys():
     private_key = RSA.generate(1024)
     public_key = private_key.publickey()
+
+    private_key_pem = private_key.export_key(format='PEM')
+    public_key_pem = public_key.export_key(format='PEM')
+    print(private_key_pem, public_key_pem)
+    return private_key_pem, public_key_pem
+
+if __name__ == "__main__":
+    generate_keys()
