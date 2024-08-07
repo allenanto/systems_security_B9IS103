c7c6ec67881c83281e13dac9085d2cf9b22f13c2
Edwin1708 <164748957+Edwin1708@users.noreply.github.com>
2024-08-07 15:31:56 +0000

    mail configuration

--- config.py
+++ config.py
@@ -7,5 +7,9 @@ class Config:
     SECRET="SECRET"
     DATABASE_NAME="Chatapp"
     MONGODB_HOST="mongodb+srv://allenantony26:allenanto@cluster0.anlxuzy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MAIL_SERVER='smtp.example.com'
+    MAIL_PORT=587
+    MAIL_USE_TLS=True
+    MAIL_USE_SSL=False
     MAIL_USERNAME=os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD=os.getenv('MAIL_PASSWORD')
